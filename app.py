0af2aafd30b28f967647cb05c781f8950bafd2f0
ephraim1029 <93262630+ephraim1029@users.noreply.github.com>
2022-02-03 15:24:20 +0630

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 #importing required modules
-import pandas as pd
 import numpy as np
-from sklearn import datasets
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn.datasets
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, precision_score, f1_score, recall_score
 
 #loading datasets from sklearn
 breast_cancer_dataset= datasets.load_breast_cancer()
