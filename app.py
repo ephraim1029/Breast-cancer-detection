435cf31763de2968ff04e9d678a06505d85987de
ephraim1029 <93262630+ephraim1029@users.noreply.github.com>
2022-02-03 16:09:12 +0630

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 #importing required modules
 import numpy as np
 import pandas as pd
-sudo pip install sklearn
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
